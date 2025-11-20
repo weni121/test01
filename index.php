c62233eecbefb389da66dd76b9c2dcf196b41b83
weni121 <weni1212312121@gmail.com>
2025-11-20 10:01:43 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        echo"hello, World";
+        echo"<br>";
+        echo"<h2>My Name is Wenisa seema.</h2>";
+        echo 'php version:'.phpversion();
+        //phpinfo();
+    ?>
+</body>
+</html>
